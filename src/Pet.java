f15fde7713bb35ea554f617c2c7f7f580d33c876
Rayonnahill1 <hill_rayonna@students.columbusstate.edu>
2025-02-07 14:32:04 -0500

    Add files via upload

--- src/Pet.java
+++ src/Pet.java
@@ -1,36 +1,81 @@
 public class Pet {
-    // private data field
+    // Private data fields
     private String name;
+    private String type;
+    private int age;
 
-    // public constructor
+    // Default constructor
     public Pet() {
-        setName("Pet Name");
+        this.name = "Pet Name";
+        this.type = "Animal";
+        this.age = 1;
     }
 
-    // mutator  method
+    // Custom constructor
+    public Pet(String name, String type, int age) {
+        this.name = name;
+        this.type = type;
+        this.age = age;
+    }
+
+    // Mutator methods
     public void setName(String name) {
         this.name = name;
     }
 
-    // accessor method
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Accessor methods
     public String getName() {
         return name;
     }
 
-    // toString method
-    public String toString() {
-        return "Pet information:\nName: " + name;
+    public String getType() {
+        return type;
     }
 
-    // main
+    public int getAge() {
+        return age;
+    }
+
+    // speak method
+    public String speak() {
+        if (type.equalsIgnoreCase("dog")) {
+            return "Woof";
+        } else if (type.equalsIgnoreCase("cat")) {
+            return "Meow";
+        } else {
+            return "Yowl";
+        }
+    }
+
+    // toString method
+    public String toString() {
+        return "Pet information:\n" +
+                "Type: " + type + "\n" +
+                "Name: " + name + "\n" +
+                "Sound: " + speak() + "\n" +
+                "Age: " + age;
+    }
+
+    // main method
     public static void main(String[] args) {
-        // First Pet object using default constructor
-        Pet pet1 = new Pet();
+        // First pet using default constructor
+        Pet pet1 = new Pet("Duke", "Dog", 9);
         System.out.println(pet1);
 
-        // setting name of pet
-        Pet pet2 = new Pet();
-        pet2.setName("Bella");
+        // Second pet using custom constructor
+        Pet pet2 = new Pet("Bella", "Dog", 11);
         System.out.println("\n" + pet2);
+
+        // Third pet using user input
+        Pet pet3 = new Pet("Sara", "Cat", 13);
+        System.out.println("\n" + pet3);
     }
 }
