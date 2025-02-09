fde95aa70a3e520bfb7c5aaccf75428add43c197
Rayonnahill1 <hill_rayonna@students.columbusstate.edu>
2025-02-09 17:54:54 -0500

    Add files via upload

--- Pet.java
+++ Pet.java
@@ -63,19 +63,4 @@ public class Pet {
                 "Sound: " + speak() + "\n" +
                 "Age: " + age;
     }
-
-    // main method
-    public static void main(String[] args) {
-        // First pet using default constructor
-        Pet pet1 = new Pet("Duke", "Dog", 9);
-        System.out.println(pet1);
-
-        // Second pet using custom constructor
-        Pet pet2 = new Pet("Bella", "Dog", 11);
-        System.out.println("\n" + pet2);
-
-        // Third pet using user input
-        Pet pet3 = new Pet("Sara", "Cat", 13);
-        System.out.println("\n" + pet3);
-    }
 }
