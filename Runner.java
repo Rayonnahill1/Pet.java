f313d12b2bd895af8677c95217d2985fc9407074
Rayonnahill1 <hill_rayonna@students.columbusstate.edu>
2025-02-25 19:30:29 -0500

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,8 +1,10 @@
-
+import java.util.Scanner;
 
 public class Runner {
     // main method
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         // First pet using default constructor
         Pet pet1 = new Pet("Duke", "Dog", 9);
         System.out.println(pet1);
@@ -12,7 +14,18 @@ public class Runner {
         System.out.println("\n" + pet2);
 
         // Third pet using user input
-        Pet pet3 = new Pet("Sara", "Cat", 13);
+        System.out.println("\nEnter pet name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter pet type: ");
+        String type = scanner.nextLine();
+
+        System.out.println("Enter pet age: ");
+        int age = scanner.nextInt();
+
+        Pet pet3 = new Pet(name, type, age);
         System.out.println("\n" + pet3);
+
+        scanner.close();
     }
 }
